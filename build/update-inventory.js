18b78befae2d61e666dca64382309e95ab347d54
collnwalkr <collinwalkerdesigns@gmail.com>
2016-12-07 17:35:33 -0800

    Can now bring in shopify products and visualize data

--- build/update-inventory.js
+++ build/update-inventory.js
@@ -1,6 +1,46 @@
+/**
+ * Script used update products from Shopify
+ *
+ * Input:   api credentials from Shopify private web app
+ * Output:  JSON file of all products
+ */
+
+
+var https = require('https')
+var fs = require('fs')
 
 var api  = require('../.api-credentials.json')
+// https://apikey:password@hostname/admin/resource.json
+var req_url  = 'https://' + api.key + ':' + api.password + '@' + api.store + '.myshopify.com/admin/'
 
-console.log(api.key);
 
+function getProducts(req_url){
+
+  https.get(req_url + 'products.json?fields=id,images,title,product_type', function(res) {
+
+    var data = [];
+
+    res.on('data', function(chunk) {
+      data.push(chunk)
+    });
+    res.on('end', function() {
+      writeToFile(JSON.parse(data))
+    });
+
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message)
+  })
+
+}
+
+function writeToFile(string){
+
+  fs.writeFile('products.json', JSON.stringify(string, null, 4 ), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+  })
+}
+
+getProducts(req_url);
 
